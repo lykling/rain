0f5219e8010f0305caf5fb50f8b660244205025e
Pride Leong <lykling.lyk@gmail.com>
2021-03-30 01:55:00 +0800

    Add deploy function
    Signed-off-by: Pride Leong <lykling.lyk@gmail.com>

--- run.sh
+++ run.sh
@@ -10,9 +10,19 @@ EOF
 " > ${target} 2> /dev/null
 }
 
+function fixpermission {
+    find /var/www/html -type d -exec chmod 755 {} \;
+    find /var/www/html -type f -exec chmod 644 {} \;
+}
+
 function launch {
     if [[ -f "/rainloop/.env.default"  ]]; then
         source /rainloop/.env.default
+    fi
+    if [[ ! -f /var/www/html/data/VERSION ]]; then
+        mkdir -pv /var/www/html
+        tar -zc -C /rainloop/rainloop . | tar -zx -C /var/www/html
+        fixpermission
     fi
     subenv /rainloop/nginx.conf.template /etc/nginx/conf.d/default.conf
     nginx -g "daemon off;"
